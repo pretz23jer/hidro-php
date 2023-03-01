402fcf72a5eb3ff0f8f2210904bbea1b5a90c19e
Walter Pretzantzín <124406002+pretz23jer@users.noreply.github.com>
2023-02-28 21:48:28 -0600

    agregando dirección del git

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/inventario/';
-
+$config['base_url'] = 'https://pretz23jer.github.io/hidro-php/';
 /*
 |--------------------------------------------------------------------------
 | Index File
