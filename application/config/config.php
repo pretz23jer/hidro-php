19c874400b0c4545f7989684937816703287c1b9
Walter Pretzantzín <124406002+pretz23jer@users.noreply.github.com>
2023-03-01 12:15:33 -0600

    Update config.php
    Agregano direccion

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'https://pretz23jer.github.io/hidro-php/';
+$config['base_url'] = 'http://www.hidrocompras.com/';
 
 /*
 |--------------------------------------------------------------------------
